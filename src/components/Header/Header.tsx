53f5294bfd18953987fec7aaca0ccc516d802e4d
Aram32mm <a01657142@tec.mx>
2024-05-05 19:10:46 -0600

    responsiveness

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import { Link, useLocation } from "react-router-dom";
 import { ROUTES } from "../../routes/constants";
 import classNames from "classnames";
 import { useState } from "react";
+import { ReactComponent as BurgerIcon} from '../../assets/burger.svg';
 
 const Navbar = () => {
   const location = useLocation();
@@ -20,52 +21,54 @@ const Navbar = () => {
 
   return (
     <header>
-      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-6 dark:bg-gray-800">
+      <nav className={`px-4 lg:px-6 py-6 dark:bg-gray-800 ${menuOpen ? 'bg-gray-800' : 'bg-white'} border-gray-200`}>
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <Link to={ROUTES.HOME}>
-            <span className="self-center text-xl font-bold whitespace-nowrap dark:text-white">
+            <span className={`self-center text-xl font-bold whitespace-nowrap ${menuOpen ? 'text-white' : 'text-black'} dark:text-white`}>
               MOVIES DB
             </span>
           </Link>
           <div className="flex justify-end lg:hidden">
             <button
-              className="block text-gray-500 hover:text-black focus:text-black focus:outline-none"
+              className={`block ${menuOpen ? 'bg-gray-800' : 'bg-white'} hover:bg-red-500 rounded-md p-2`}
               id="mobile-menu-button"
               onClick={toggleMenu}
             >
-              <h1>Menu</h1>
+              <BurgerIcon className="w-4 h-4 fill-current" />
             </button>
           </div>
-          <div
-            className={`justify-between items-center w-full lg:flex lg:order-1 ${
-              menuOpen ? "block" : "hidden"
-            }`}
-            id="desktop-menu"
-          >
-            <ul className="flex mt-0 font-medium flex-row space-x-8">
-              {navItems.map((item) => {
-                const navClass = classNames({
-                  "block py-2 pr-4 pl-3 rounded bg-transparent p-0": true,
-                  "text-white": location.pathname !== item.path,
-                  "text-red-500": location.pathname === item.path,
-                });
-                return (
-                  <li key={item.id}>
-                    <Link
-                      to={item.path}
-                      className={navClass}
-                      aria-current="page"
-                    >
-                      {item.name}
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+        </div>
+        <div
+          className={`justify-between items-center w-full lg:flex lg:order-1 ${
+            menuOpen ? "block" : "hidden"
+          }`}
+          id="desktop-menu"
+        >
+          <ul className="flex mt-0 font-medium flex-row space-x-8">
+            {navItems.map((item) => {
+              const navClass = classNames({
+                "block py-2 pr-4 pl-3 rounded bg-transparent p-0": true,
+                "text-white": location.pathname !== item.path,
+                "text-red-500": location.pathname === item.path,
+              });
+              return (
+                <li key={item.id}>
+                  <Link
+                    to={item.path}
+                    className={navClass}
+                    aria-current="page"
+                  >
+                    {item.name}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </nav>
     </header>
+
+
   );
 };
 
