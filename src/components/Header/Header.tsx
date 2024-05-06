b5305a3f053eff8d352e6bf3610c8b1c8962162a
Aram32mm <a01657142@tec.mx>
2024-05-05 18:50:20 -0600

    responsiveness

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,11 @@
 import { Link, useLocation } from "react-router-dom";
-
 import { ROUTES } from "../../routes/constants";
 import classNames from "classnames";
+import { useState } from "react";
 
 const Navbar = () => {
   const location = useLocation();
-  console.log(location.pathname);
+  const [menuOpen, setMenuOpen] = useState(false);
   const navItems = [
     { id: 1, name: "Home", path: ROUTES.HOME },
     { id: 2, name: "Popular", path: ROUTES.POPULAR },
@@ -13,6 +13,10 @@ const Navbar = () => {
     { id: 4, name: "Now Playing", path: ROUTES.NOW_PLAYING },
     { id: 5, name: "My Favorites", path: ROUTES.FAVORITES },
   ];
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <header>
@@ -23,9 +27,20 @@ const Navbar = () => {
               MOVIES DB
             </span>
           </Link>
+          <div className="flex justify-end lg:hidden">
+            <button
+              className="block text-gray-500 hover:text-black focus:text-black focus:outline-none"
+              id="mobile-menu-button"
+              onClick={toggleMenu}
+            >
+              <h1>Menu</h1>
+            </button>
+          </div>
           <div
-            className="justify-between items-center flex w-auto order-1"
-            id="mobile-menu-2"
+            className={`justify-between items-center w-full lg:flex lg:order-1 ${
+              menuOpen ? "block" : "hidden"
+            }`}
+            id="desktop-menu"
           >
             <ul className="flex mt-0 font-medium flex-row space-x-8">
               {navItems.map((item) => {
